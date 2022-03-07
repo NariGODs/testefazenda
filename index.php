3257d374192ae4164039ab6e9a7cddfba4e449c6
Felipe Teles <felipe.teles@outlook.com.br>
2022-03-07 20:33:48 -0300

    Teste 1

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "OIIIIIIIIIIIIIIIIII !!!!!!!!! Obs.: ME MAMA GOSTOSA !!!";
+class login {
+  private $numerocamera;
+  private $descricao;
+
+  public function getUser($numerocamera) {
+    if($this->numerocamera == "1"):
+      $descricao = "Câmera FB-2000";
+    else if($this->numerocamera == "2")
+      $descricao = "Câmera A900";
+    endif;
+    return $this->descricao;
+  }
+}
